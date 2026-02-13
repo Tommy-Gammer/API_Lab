3b3bb1c1732b95fe62d3f1deb6f3e2d56bc7584b
tommy gammer <tgammer0415@gmail.com>
2026-02-13 00:34:57 -0500

    Finished National Parks App

--- app/src/main/java/com/codepath/nationalparks/NationalPark.kt
+++ app/src/main/java/com/codepath/nationalparks/NationalPark.kt
@@ -9,24 +9,28 @@ import com.google.gson.annotations.SerializedName
  * object to correctly parse with the gson library.
  */
 class NationalPark {
-
-    // Name field
     @JvmField
     @SerializedName("fullName")
     var name: String? = null
 
-    // Description field
     @JvmField
     @SerializedName("description")
     var description: String? = null
 
-    // Location or State field
     @JvmField
     @SerializedName("states")
     var location: String? = null
 
-    //TODO parkImageUrl
+    @SerializedName("images")
+    var images: List<Image>? = null
 
+    // Convenience property to access the first image’s URL
+    val imageUrl: String? get() = images?.firstOrNull()?.url
+
+    class Image {
+        @SerializedName("url")
+        var url: String? = null
+    }
+}
 
     //TODO-STRETCH-GOALS
-}
--- app/src/main/java/com/codepath/nationalparks/NationalParksFragment.kt
+++ app/src/main/java/com/codepath/nationalparks/NationalParksFragment.kt
@@ -1,6 +1,7 @@
 package com.codepath.nationalparks
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +10,18 @@ import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.Headers
+import org.json.JSONArray
 
 // --------------------------------//
 // CHANGE THIS TO BE YOUR API KEY  //
 // --------------------------------//
-private const val API_KEY = "<YOUR-API-KEY-HERE>"
+private const val API_KEY = ""
 
 /*
  * The class for the only fragment in the app, which contains the progress bar,
@@ -22,23 +29,23 @@ private const val API_KEY = "<YOUR-API-KEY-HERE>"
 
  */
 class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
-        /*
+    /*
      * Constructing the view
      */
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            val view = inflater.inflate(R.layout.fragment_national_parks_list, container, false)
-            val progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
-            val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
-            val context = view.context
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_national_parks_list, container, false)
+        val progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
+        val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
+        val context = view.context
 
-            recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = LinearLayoutManager(context)
 
-            updateAdapter(progressBar, recyclerView)
-            return view
-        }
+        updateAdapter(progressBar, recyclerView)
+        return view
+    }
 
     /*
      * Updates the RecyclerView adapter with new data.  This is where the
@@ -48,53 +55,66 @@ class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
         progressBar.show()
 
         // Create and set up an AsyncHTTPClient() here
+        val client = AsyncHttpClient()
+        val params = RequestParams()
+        params["api_key"] = API_KEY
 
         // Using the client, perform the HTTP request
+        client[
+            "https://developer.nps.gov/api/v1/parks",
+            params,
+            object : JsonHttpResponseHandler() { // connect these callbacks to your API call
 
-        /* Uncomment me once you complete the above sections!
-        {
-            /*
-             * The onSuccess function gets called when
-             * HTTP response status is "200 OK"
-             */
-            override fun onSuccess(
-                statusCode: Int,
-                headers: Headers,
-                json: JsonHttpResponseHandler.JSON
-            ) {
-                // The wait for a response is over
-                progressBar.hide()
+                /*
+                 * The onSuccess function gets called when
+                 * HTTP response status is "200 OK"
+                 */
+                override fun onSuccess(
+                    statusCode: Int,
+                    headers: Headers,
+                    json: JsonHttpResponseHandler.JSON
+                ) {
+                    // The wait for a response is over
+                    progressBar.hide()
 
-                //TODO - Parse JSON into Models
+                    val dataJSON = json.jsonObject.get("data") as JSONArray
+                    val parksRawJSON = dataJSON.toString()
 
-                val models : List<NationalPark> = mutableListOf() // Fix me!
-                recyclerView.adapter = NationalParksRecyclerViewAdapter(models, this@NationalParksFragment)
+                    // Create a Gson instance to help parse the raw JSON
+                    val gson = Gson()
 
-                // Look for this in Logcat:
-                Log.d("NationalParksFragment", "response successful")
-            }
+                    // Tell Gson what type we’re expecting (a list of NationalPark objects)
+                    val arrayParkType = object : TypeToken<List<NationalPark>>() {}.type
 
-            /*
-             * The onFailure function gets called when
-             * HTTP response status is "4XX" (eg. 401, 403, 404)
-             */
-            override fun onFailure(
-                statusCode: Int,
-                headers: Headers?,
-                errorResponse: String,
-                t: Throwable?
-            ) {
-                // The wait for a response is over
-                progressBar.hide()
+                    // Convert the raw JSON string into a list of actual NationalPark data models
+                    val models: List<NationalPark> = gson.fromJson(parksRawJSON, arrayParkType)
 
-                // If the error is not null, log it!
-                t?.message?.let {
-                    Log.e("NationalParksFragment", errorResponse)
+                    recyclerView.adapter = NationalParksRecyclerViewAdapter(models, this@NationalParksFragment)
+
+                    // Look for this in Logcat:
+                    Log.d("NationalParksFragment", "response successful")
+                }
+
+                /*
+                 * The onFailure function gets called when
+                 * HTTP response status is "4XX" (eg. 401, 403, 404)
+                 */
+                override fun onFailure(
+                    statusCode: Int,
+                    headers: Headers?,
+                    errorResponse: String,
+                    t: Throwable?
+                ) {
+                    // The wait for a response is over
+                    progressBar.hide()
+
+                    // If the error is not null, log it!
+                    t?.message?.let {
+                        Log.e("NationalParksFragment", errorResponse)
+                    }
                 }
             }
-        }]
-        */
-
+        ]
     }
 
     /*
@@ -103,5 +123,4 @@ class NationalParksFragment : Fragment(), OnListFragmentInteractionListener {
     override fun onItemClick(item: NationalPark) {
         Toast.makeText(context, "Park Name: " + item.name, Toast.LENGTH_LONG).show()
     }
-
 }
